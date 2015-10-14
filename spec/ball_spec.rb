4af163f4e8dc0d966631dbac81346f0711c630b3
galulex <alex@active-bridge.com>
2015-10-14 22:23:14 +0300

    Add specs

--- spec/ball_spec.rb
+++ spec/ball_spec.rb
@@ -4,10 +4,15 @@ require_relative '../ball'
 RSpec.describe Ball do
   let(:answers) { YAML.load_file(File.join(__dir__, '../answers.yml')) }
 
-  it 'prints the answer' do
-    expect(STDOUT).to receive(:puts)
-    subject.shake
-  end
-
   it { expect(answers).to include(subject.shake) }
+  it { expect(answers).to eql(Ball::ANSWERS) }
+
+  describe '#shake' do
+    before { stub_const('Ball::ANSWERS', ['ANSWER']) }
+
+    it 'prints colorized answer' do
+      expect(STDOUT).to receive(:puts).with("\e[31mANSWER\e[0m")
+      subject.shake
+    end
+  end
 end
